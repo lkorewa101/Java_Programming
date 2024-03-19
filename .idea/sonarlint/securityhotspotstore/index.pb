9719525778592d70a0beadf22c5d0c4badb504ce
Kihyun Ko <lkorewa101@gmail.com>
2024-03-19 14:27:45 +0900

    first

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,9 @@
 
 =
 src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+;
+notepad.iml,1\4\14278eeee0b2988b0a571e0cf6334db03f82b1af
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] arg) {
+        Scanner s = new Scanner(System.in);
+
+        float op1 = s.nextInt();
+        String op = s.next();
+        float op2 = s.nextInt();
+
+        float result = 0;
+
+        if(op.equals("+")) {
+            result = op1 + op2;
+        } else if(op.equals("-")) {
+            result = op1 - op2;
+        } else if(op.equals("*")) {
+            result = op1 * op2;
+        } else if(op.equals("/")) {
+            if(op2 == 0) {
+                System.out.println("False");
+                s.close();
+                return;
+            } else {
+                result = op1 / op2;
+            }
+        }
+
+        System.out.println(op1 + " " + op + " " + op2 + " = " + result);
+        s.close();
+        }
     }
-}
